39aeef5f3141f7d78c7f484b0c1c1c5ce3630a91
felipe0703 <felipe.aguilera.g@gmail.com>
2020-08-14 02:46:35 -0400

    archivo docs para subir a github

---
+++ docs/precache-manifest.900253f13e929bfa6554099da4ce055e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d02405ef24b8c21f77294163bd8323f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b30f3999fd87b9c4e506",
+    "url": "/static/css/main.a889d10a.chunk.css"
+  },
+  {
+    "revision": "5a826f86ef620ea49d3a",
+    "url": "/static/js/2.19a262ac.chunk.js"
+  },
+  {
+    "revision": "388e667d555f121bdc71ab5c34de28f9",
+    "url": "/static/js/2.19a262ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30f3999fd87b9c4e506",
+    "url": "/static/js/main.1fae0f30.chunk.js"
+  },
+  {
+    "revision": "45af19b57e5f19e50f15",
+    "url": "/static/js/runtime-main.c6046e18.js"
+  }
+]);
